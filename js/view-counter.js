926fd6af0989fdb93539301839e862c342a97d7e
Eldar-005 <eldarhamidov2009@gmail.com>
2026-06-14 15:28:00 +0400

    Move view count to navbar (right of Our Team), add mobile support

--- js/view-counter.js
+++ js/view-counter.js
@@ -1,11 +1,14 @@
-const viewCountEl = document.getElementById('view-count');
-if (viewCountEl) {
+const updateViewCount = () => {
   fetch('https://countapi.mileshilliard.com/api/v1/hit/aqua-fly-visits')
     .then(res => res.json())
     .then(data => {
-      viewCountEl.textContent = `${data.value} views`;
+      const text = `${data.value} views`;
+      const el = document.getElementById('view-count');
+      const mobileEl = document.getElementById('mobile-view-count');
+      if (el) el.textContent = text;
+      if (mobileEl) mobileEl.textContent = text;
     })
-    .catch(() => {
-      viewCountEl.textContent = '';
-    });
-}
+    .catch(() => {});
+};
+
+updateViewCount();
