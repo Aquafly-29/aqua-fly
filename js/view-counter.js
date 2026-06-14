2a0a55175867ba2ac2055a8a98fc4ecefd7a3ce1
Eldar-005 <eldarhamidov2009@gmail.com>
2026-06-14 15:21:08 +0400

    Fix view counter API - countapi.xyz is dead, replace with working alternative

--- js/view-counter.js
+++ js/view-counter.js
@@ -1,6 +1,6 @@
 const viewCountEl = document.getElementById('view-count');
 if (viewCountEl) {
-  fetch('https://api.countapi.xyz/hit/aqua-fly-static/visits')
+  fetch('https://countapi.mileshilliard.com/api/v1/hit/aqua-fly-visits')
     .then(res => res.json())
     .then(data => {
       viewCountEl.textContent = `${data.value} views`;
